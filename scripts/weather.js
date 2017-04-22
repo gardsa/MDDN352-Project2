4f4905e4afc3eeadad79b479c052f2133f4b0576
Samantha Gard <sam.gard@abletech.co.nz>
2017-04-22 14:42:23 +1200

    Added modular grid and relevant scripts Rendering location and weather description from OpenWeatherMaps JSON response

--- scripts/weather.js
+++ scripts/weather.js
@@ -17,10 +17,18 @@ function getWeatherData(lat, lng) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', owmBaseURL + "lat=" + lat + "&lon=" + lng + "&APPID=" + owmAPIKey);
   xhr.addEventListener('load', function(event) {
-    let weatherData = JSON.parse(event.target.response);
-    console.log(weatherData);
+    let data = JSON.parse(event.target.response);
+    console.log(data);
+    renderWeatherInfo(data);
   });
   xhr.send();
+}
+
+function renderWeatherInfo(data) {
+  var location = document.getElementById('location'),
+      descriptionElem = document.getElementById('description');
+  location.innerHTML = data.name;
+  description.innerHTML = data.weather[0].description;
 }
 
 document.addEventListener('DOMContentLoaded', function(){
