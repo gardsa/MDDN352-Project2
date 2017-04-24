a134d00ee46486ddd00139d660b7df5b450f10e5
Samantha Gard <sam.gard@abletech.co.nz>
2017-04-24 16:45:22 +1200

    Added dropdown home screen settings menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,13 @@
 (function(d){
-  function toggleMenuClass(){
+  function toggleMenuClass() {
     d.body.classList.toggle('slideout-open');
   }
   d.getElementById('menu-icon').addEventListener('click', toggleMenuClass);
-  d.getElementById('mask').addEventListener('click', toggleMenuClass);
+  d.getElementById('menu-mask').addEventListener('click', toggleMenuClass);
+
+  function toggleSettingsClass() {
+    d.body.classList.toggle('dropdown-open');
+  }
+  d.getElementById('settings-icon').addEventListener('click', toggleSettingsClass);
+  d.getElementById('settings-mask').addEventListener('click', toggleSettingsClass);
 })(document);
